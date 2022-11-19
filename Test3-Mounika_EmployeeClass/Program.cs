fd4305f9e5b8f9b832267993b7cb9ce3428da441
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-11-19 12:51:02 +0530

    Add project files.

---
+++ Test3-Mounika_EmployeeClass/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Test3_Mounika_EmployeeClassLibrary;
+
+SalaryCalculation objSalaryCalculation = new SalaryCalculation();
+Console.WriteLine("Enter Employee Details.....");
+Console.WriteLine("Enter Employee Id");
+objSalaryCalculation.EmployeeId = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Employee Name");
+objSalaryCalculation.EmployeeName = Console.ReadLine();
+
+Console.WriteLine("Enter Employee Salary");
+objSalaryCalculation.EmployeeSalary = int.Parse(Console.ReadLine());
+
+objSalaryCalculation.DisplayEmployeeDetails();
+objSalaryCalculation.TaxCalculation();
+objSalaryCalculation.DisplaySalaryDetails();
